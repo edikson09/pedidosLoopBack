61e05059fe41d9d8795f256cff708b64c81a9494
edikson09 <ediksonjr@gmail.com>
2022-11-08 19:42:56 -0500

    2

--- src/config/llaves.ts
+++ src/config/llaves.ts
@@ -3,5 +3,5 @@ export namespace Llaves {
   /*reemplazar por url herocu*/
   export const urlServicioCorreo ='http://127.0.0.1:5000';
   /*reemplazar cadena de conexcion de datasour*/
-  export const cadenaConexion =  'mongodb+srv://ediksonjr09:Nico0603@ediksonrodriguez.zss4vpf.mongodb.net/?retryWrites=true&w=majority'
+  export const cadenaConexion =  'mongodb+srv://ediksonjr09:Nico0603@ediksonrodriguez.zss4vpf.mongodb.net/PedidosBDG36retryWrites=true&w=majority'
 }
